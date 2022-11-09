92b5b8a95f3f7103e21ce87365f65caea18e3993
sabithanazareth <41137183+sabithanazareth@users.noreply.github.com>
2022-11-09 09:50:01 -0500

    Update Date and Task Priority in form

--- src/app/components/create-task/form/form.component.ts
+++ src/app/components/create-task/form/form.component.ts
@@ -1,11 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import * as moment from 'moment';
 import { CreateTaskComponent } from '../create-task.component';
 
 @Component({
@@ -22,7 +21,6 @@ export class FormComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public editData: any
   ) {
     const currentDay = new Date();
-    // let date = moment(currentDay, "MM/DD/YYYY");
     this.minDate = currentDay;
   }
 
@@ -37,23 +35,13 @@ export class FormComponent implements OnInit {
       date: ['', Validators.required],
       description: ['', Validators.required],
     });
-    // console.log(this.editData);
-    // console.log(this.form.controls['date']);
-    // let date = new Date(this.editData.date_completed);
-    // console.log(this.editData.created_at);
-    // console.log(date.toUTCString());
-    // console.log(date.toLocaleString());
-    // let dddd = date.toUTCString();
-    // date.setHours(this.editData.date_completed.getHours() + 24);
-    // console.log(dddd);
-    // console.log(date);
 
     if (this.editData) {
-      console.log(this.form.controls['taskPriority']);
-      console.log(this.form.controls['date']);
       this.form.controls['taskName'].setValue(this.editData.task_name);
       this.form.controls['points'].setValue(this.editData.points);
-      this.form.controls['taskPriority'].setValue(this.editData.task_priority);
+      this.form.controls['taskPriority'].setValue(
+        this.editData.task_priority.toString()
+      );
       this.form.controls['date'].setValue(this.editData.date_completed);
       this.form.controls['description'].setValue(this.editData.description);
     }
@@ -62,7 +50,6 @@ export class FormComponent implements OnInit {
     let data = this.form.value;
     let date = data.date;
     date.setHours(23, 59, 0);
-    console.log(date.toISOString());
     let postData = {
       task_name: data.taskName,
       points: data.points,
